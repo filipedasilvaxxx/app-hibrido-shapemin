42c530bada75c73d3fe2b1a9fc0195fca10a151a
filipedasilvaxxx <vasconselossilva791@gmail.com>
2019-08-09 18:58:34 -0300

    0.1

--- src/app/tribulus-list/tribulus-list.module.ts
+++ src/app/tribulus-list/tribulus-list.module.ts
@@ -23,4 +23,22 @@ const routes: Routes = [
   ],
   declarations: [TribulusListPage]
 })
+
+
 export class TribulusListPageModule {}
+
+const menuCtrl = document.querySelector('ion-menu-controller');
+
+function openFirst() {
+  menuCtrl.enable(true, 'first');
+  menuCtrl.open('first');
+}
+
+function openEnd() {
+  menuCtrl.open('end');
+}
+
+function openCustom() {
+  menuCtrl.enable(true, 'custom');
+  menuCtrl.open('custom');
+}
